e19bc057560fd784d59967c798c99517c39eb0fa
Osipovsavely <145055872+Osipovsavely@users.noreply.github.com>
2025-03-30 15:20:22 +0300

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Image interfase, buttum_next;
+    [SerializeField] int weapon, stone, food, population;
+    [SerializeField] int wall_HP = 1000;
+    [SerializeField] TMP_Text weapont, stonet, foodt, populationt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        weapont.text = weapon.ToString();
+        stonet.text = stone.ToString();
+        foodt.text = food.ToString();
+        populationt.text = population.ToString();
+    }
+
+
+}
---
+++ Assets/Scripts/step.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s : MonoBehaviour
+{
+    [SerializeField] int steps = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void step()
+    {
+        steps =+ 1;
+        //food =+ food_day
+    }
+}
