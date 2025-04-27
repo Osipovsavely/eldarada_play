9f87dd57d5ab679bdc3d74b659b628c3169aa5f8
Osipovsavely <145055872+Osipovsavely@users.noreply.github.com>
2025-04-27 16:33:46 +0300

    wall part 2

--- Assets/Scripts/Playerdata.cs
+++ Assets/Scripts/Playerdata.cs
@@ -6,13 +6,25 @@ public static class Playerdata
 {
     public static int res_contr, rescon;
     public static float step;//ход
-    public static float stone, stone_day;//камень
-    public static float weapon, weapon_day;//оружие
-    public static float food, food_day;//еда
-    public static float population, population_day;//население
+    public static float stone_day;//камень
+    public static float weapon_day;//оружие
+    public static float food_day;//еда
+    public static float population_day;//население
     public static int up_stoimost_stone, up_stoimost_people, up_stoimost_food;
     //public static int minus_food
 
+    public static float stone = 200;
+    public static float weapon = 200;
+    public static float food = 10000;
+    public static float population = 500;
+
     //Для стены
-    public static int weapon_HP, people_HP, stone_HP;
+    
+    public static int stone_HP = 1000; 
+    public static int weapon_HP = 200;
+    public static int people_HP = 200;
+    public static int stone_HP_max = 10000;
+    public static int people_HP_max = 3000;
+    public static int weapon_HP_max = 2000;
+    public static int pechat_people_full, pechat_stone_full, pechat_weapon_full;
 }
--- Assets/Scripts/Wall_logic.cs
+++ Assets/Scripts/Wall_logic.cs
@@ -5,13 +5,55 @@ using TMPro;
 
 public class Wall_logic : MonoBehaviour
 {
-    [SerializeField] GameObject panelwall;
-    [SerializeField] TMP_Text stoneHP, weaponHP, peopleHP;
-    int stone_HP_max = 1000;
+    [SerializeField] GameObject panelwall, pechat_stone, pechat_weapon, pechat_people;
+    [SerializeField] TMP_Text stoneHP_text, weaponHP_text, peopleHP_text;
+    //int stone_HP_max = 10000;
+    //int people_HP_max = 3000;
+    //int weapon_HP_max = 2000;
+    //int stone_HP, weapon_HP, people_HP;
     //stoneHP.text = Playerdata.stone_HP.ToString();
     
-    void Start()
-    {
 
+    
+    void Update()
+    {
+        stoneHP_text.text = Playerdata.stone_HP + "/" + Playerdata.stone_HP_max;
+        weaponHP_text.text = Playerdata.weapon_HP + "/" + Playerdata.weapon_HP_max;
+        peopleHP_text.text = Playerdata.people_HP + "/" + Playerdata.people_HP_max;
+    }
+
+    public void Wall_UP_stone()
+    {
+        if ((Playerdata.stone >= 400) && (Playerdata.pechat_stone_full != 1))
+        {
+            Playerdata.stone_HP += 400;
+            Playerdata.stone -= 400;
+            pechat_stone.SetActive(true);
+            Playerdata.pechat_stone_full = 1;
+        }
+
+        
+        
+    }
+    public void Wall_UP_people()
+    {  
+        if ((Playerdata.population >= 200) && (Playerdata.pechat_people_full != 1))
+        {
+            Playerdata.people_HP += 200;
+            Playerdata.population -= 200;
+            pechat_people.SetActive(true);
+            Playerdata.pechat_people_full = 1;
+        }
+        
+    }
+    public void Wall_UP_weapon()
+    {
+        if ((Playerdata.weapon >= 100) && (Playerdata.pechat_weapon_full != 1))
+        {
+            Playerdata.weapon_HP += 100;
+            Playerdata.weapon -= 100;
+            pechat_weapon.SetActive(true);
+            Playerdata.pechat_weapon_full = 1;
+        }
     }
 }
